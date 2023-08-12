dde2359eb5ee0c7559a083b6df62a237eef537c6
Tomk99 <tcsavas99@gmail.com>
2023-08-12 12:58:49 +0200

    Display Genres

--- src/App.tsx
+++ src/App.tsx
@@ -10,17 +10,21 @@ function App() {
         base: `"nav" "main"`,
         lg: `"nav nav" "aside main"`,
       }}
+      templateColumns={{
+        base: "1fr",
+        lg: '200px 1fr'
+      }}
     >
       <GridItem area="nav">
-        <NavBar/>
+        <NavBar />
       </GridItem>
       <Show above="lg">
-        <GridItem area="aside">
-          <GenreList/>
+        <GridItem area="aside" paddingX={5}>
+          <GenreList />
         </GridItem>
       </Show>
       <GridItem area="main">
-        <GameGrid/>
+        <GameGrid />
       </GridItem>
     </Grid>
   );
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,6 +1,3 @@
-import { useEffect, useState } from "react";
-import apiClients from "../services/api-clients";
-import { CanceledError } from "axios";
 import useData from "./useData";
 
 export interface Platform {
